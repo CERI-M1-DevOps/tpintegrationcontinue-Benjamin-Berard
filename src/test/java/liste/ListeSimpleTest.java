959ebf87d9ad2cfbf992e8d435eedcfc7d5b5eb6
uapv2307875 <benjaminb4@outlook.fr>
2024-11-07 18:04:13 +0100

    test coverage

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -227,9 +227,8 @@ public class ListeSimpleTest {
         assertEquals(listeATester.toString(), "ListeSimple(Noeud(1), Noeud(2), Noeud(3), Noeud(4), Noeud(5))");
         listeATester.echanger(r1, r2);
         assertEquals(listeATester.toString(), "ListeSimple(Noeud(1), Noeud(4), Noeud(3), Noeud(2), Noeud(5))");
-        listeATester.supprimePremier(1);
-        listeATester.echanger(r1,r2);
-        assertEquals(listeATester.toString(), "ListeSimple(Noeud(2), Noeud(3), Noeud(4), Noeud(5))");
+        listeATester.supprimerPremier(1);
+        assertEquals(r2,listeATester.tete);
     }
 
     @Test
